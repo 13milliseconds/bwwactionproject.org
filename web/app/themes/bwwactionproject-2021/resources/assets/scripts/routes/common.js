edf83d3b26301a0848043e2abca385a2f2dc1659
Francois Huyghe <francois@13milliseconds.com>
2021-06-28 21:09:15 -0400

    header

--- web/app/themes/bwwactionproject-2021/resources/assets/scripts/routes/common.js
+++ web/app/themes/bwwactionproject-2021/resources/assets/scripts/routes/common.js
@@ -1,6 +1,13 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 100) {
+        $('.banner').addClass('scrolled');
+      } else {
+        $('.banner').removeClass('scrolled');
+      }
+    })
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
