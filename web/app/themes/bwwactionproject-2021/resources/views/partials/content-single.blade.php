aa9334106400e0e0d124842fbb6623e861baf9cc
Francois Huyghe <francois@13milliseconds.com>
2021-07-01 11:46:06 -0400

    Campaign link button

--- web/app/themes/bwwactionproject-2021/resources/views/partials/content-single.blade.php
+++ web/app/themes/bwwactionproject-2021/resources/views/partials/content-single.blade.php
@@ -6,6 +6,11 @@
           <div class="category">{{ get_the_category()[0]->name }}</div>
           <h1 class="entry-title">{!! get_the_title() !!}</h1>
           {!! the_excerpt() !!}
+
+          @php $website = get_field('website') @endphp
+          @if($website)
+              <a href="{{ $website }}" class="button" target="_blank">Visit</a>
+          @endif
         </div>
         <div class="col-md-6">
           {!! the_post_thumbnail('large') !!}
